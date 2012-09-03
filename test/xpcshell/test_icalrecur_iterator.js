e115c7d1c2571df25d70eba0072975118f94c7fc
Philipp Kewisch <mozilla@kewis.ch>
2012-09-03 20:29:16 +0200

    Fix MPL header and formatting

--- test/xpcshell/test_icalrecur_iterator.js
+++ test/xpcshell/test_icalrecur_iterator.js
@@ -1,6 +1,7 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * Portions Copyright (C) Philipp Kewisch, 2011-2012 */
 
 function run_test() {
     test_init();
--- tools/gjslint.py
+++ tools/gjslint.py
@@ -1,6 +1,7 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# Portions Copyright (C) Philipp Kewisch, 2012
 
 from closure_linter import gjslint
 import icaljs_errorrules
