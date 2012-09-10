eac80205003d0319a36aad1dd2ddfd41696ae781
James Lal <james@lightsofapollo.com>
2012-09-10 10:42:57 -0700

    naming fixes, minor Time.toJSDate fix, tests for ICAL.icaltime

--- test/mocha/time_test.js
+++ test/mocha/time_test.js
@@ -1,9 +1,9 @@
-testSupport.requireICAL();
-
 suite('icaltime', function() {
+  var Time = ICAL.icaltime;
+  var Timezone = ICAL.icaltimezone;
 
   test('round trip', function() {
-    var f = new ICAL.icaltime({
+    var f = new Time({
       second: 1,
       minute: 2,
       hour: 3,
@@ -18,8 +18,175 @@ suite('icaltime', function() {
     // TODO also check UTC dates
 
     g.reset();
-    assert.equal(g, ICAL.icaltime.epoch_time.toString());
+    assert.equal(g, Time.epoch_time.toString());
   });
+
+  suite('#fromJSDate', function() {
+
+    test('utc', function() {
+      var date = new Date(2012, 0, 1);
+      var expected = {
+        year: date.getUTCFullYear(),
+        // + 1 ICAL.js is not zero based...
+        month: date.getUTCMonth() + 1,
+        day: date.getUTCDate(),
+        hour: date.getUTCHours(),
+        minute: date.getUTCMinutes(),
+        second: date.getUTCSeconds()
+      };
+
+      var subject = Time.fromJSDate(date, true);
+
+      assert.hasProperties(
+        subject, expected
+      );
+    });
+
+    test('floating', function() {
+      var date = new Date(2012, 0, 1);
+      var subject = new Time.fromJSDate(date);
+
+      assert.deepEqual(
+        subject.toJSDate(),
+        date
+      );
+    });
+  });
+
+  suite('#fromData', function() {
+    test('empty object', function() {
+      var subject = Time.fromData();
+      var expected = {
+        year: 0,
+        month: 1,
+        day: 1,
+        hour: 0,
+        minute: 0,
+        second: 0
+      };
+
+      assert.hasProperties(subject, expected, 'starts at begining of time');
+    });
+
+    test('with year, month', function() {
+      var subject = Time.fromData({
+        year: 2012,
+        month: 1
+      });
+
+      assert.hasProperties(subject, {
+        year: 2012,
+        month: 1
+      });
+    });
+
+    test('utc timezone', function() {
+      var subject = Time.fromData({
+        year: 2012,
+        timezone: 'Z'
+      });
+
+      assert.hasProperties(subject, {
+        year: 2012,
+        zone: Timezone.utc_timezone
+      });
+    });
+  });
+
+  suite('#dayOfWeek', function() {
+
+    // format for dayOfWeek assertion
+    // is [dayNumber, dateObject]
+    var assertions = [
+      [Time.SUNDAY, new Date(2012, 0, 1)],
+      [Time.MONDAY, new Date(2012, 0, 2)],
+      [Time.TUESDAY, new Date(2012, 0, 3)],
+      [Time.WEDNESDAY, new Date(2012, 0, 4)],
+      [Time.THURSDAY, new Date(2012, 0, 5)],
+      [Time.FRIDAY, new Date(2012, 0, 6)],
+      [Time.SATURDAY, new Date(2012, 0, 7)]
+      //TODO: Add more I was lazy here this is
+      //      mostly to check that the function is
+      //      sane but if there is a bug somewhere
+      //      we can add tests above...
+    ];
+
+    assertions.forEach(function(item) {
+      var dayOfWeek = item[0];
+      var date = item[1];
+      var human = date.getFullYear() + '-' + date.getMonth() + '-' + date.getDate();
+      var msg = human + ' should be #' + dayOfWeek + ' day';
+
+      test(msg, function() {
+        var subject = new ICAL.icaltime.fromJSDate(
+          date
+        );
+
+        assert.equal(
+          subject.dayOfWeek(),
+          dayOfWeek
+        );
+      });
+    });
+
+  });
+
+  //XXX: Common pattern refactor ?
+  suite('#dayOfYear', function() {
+    var inc;
+
+    function testYear(start) {
+      var end = new Date(
+        start.getFullYear() + 1,
+        start.getMonth(),
+        start.getDate()
+      );
+
+      var max = 400;
+      var cur = start;
+      var date = new Date();
+      inc = 1;
+      var time = Time.fromJSDate(cur);
+
+      end = new Date(
+        end.getFullYear(),
+        end.getMonth(),
+        0
+      );
+
+      while (end.valueOf() >= cur.valueOf()) {
+        if (inc > max) {
+          throw new Error('test error inf loop');
+          break;
+        }
+
+        assert.equal(
+          time.dayOfYear(),
+          inc,
+          cur.toString()
+        );
+
+        cur = new Date(
+          start.getFullYear(),
+          0,
+          start.getDate() + inc
+        );
+        time = Time.fromJSDate(cur);
+        inc++;
+      }
+    }
+
+    test('full year (2011/no leap)', function() {
+      testYear(new Date(2011, 0, 1));
+      assert.equal(inc - 1, 365, 'is not leap');
+    });
+
+    test('full year (2012 + leap)', function() {
+      testYear(new Date(2012, 0, 1));
+      assert.equal(inc - 1, 366, 'is leap');
+    });
+  });
+
 
   test('calculations', function() {
 
@@ -35,29 +202,29 @@ suite('icaltime', function() {
 
     for (var datakey in test_data) {
       var data = test_data[datakey];
-      var dt = ICAL.icaltime.fromString(data.str);
+      var dt = Time.fromString(data.str);
       var cp = dt.clone();
 
       assert.equal(dt.toUnixTime(), data.expect_unixtime);
-      var dur = dt.subtractDate(ICAL.icaltime.epoch_time);
+      var dur = dt.subtractDate(Time.epoch_time);
       assert.equal(dur.toSeconds(), data.expect_unixtime);
 
       cp = dt.clone();
       cp.year += 1;
       var diff = cp.subtractDate(dt);
-      var yearseconds = (365 + ICAL.icaltime.is_leap_year(dt.year)) * 86400;
+      var yearseconds = (365 + Time.is_leap_year(dt.year)) * 86400;
       assert.equal(diff.toSeconds(), yearseconds);
 
       cp = dt.clone();
       cp.year += 2;
       var diff = cp.subtractDate(dt);
-      var yearseconds = (365 + ICAL.icaltime.is_leap_year(dt.year) + 365 + ICAL.icaltime.is_leap_year(dt.year + 1)) * 86400;
+      var yearseconds = (365 + Time.is_leap_year(dt.year) + 365 + Time.is_leap_year(dt.year + 1)) * 86400;
       assert.equal(diff.toSeconds(), yearseconds);
 
       cp = dt.clone();
       cp.year -= 1;
       var diff = cp.subtractDate(dt);
-      var yearseconds = (365 + ICAL.icaltime.is_leap_year(cp.year)) * 86400;
+      var yearseconds = (365 + Time.is_leap_year(cp.year)) * 86400;
       assert.equal(diff.toSeconds(), -yearseconds);
 
       cp = dt.clone();
@@ -94,7 +261,7 @@ suite('icaltime', function() {
   });
 
   test('normalize', function() {
-    var f = new ICAL.icaltime({
+    var f = new Time({
         second: 59,
         minute: 59,
         hour: 23,
@@ -115,7 +282,7 @@ suite('icaltime', function() {
 
     for (var datakey in test_data) {
         var data = test_data[datakey];
-        var dt = ICAL.icaltime.fromString(data.str);
+        var dt = Time.fromString(data.str);
         var cur_seconds = dt.second;
         var add_seconds = data.add_seconds || 0;
 
@@ -141,8 +308,8 @@ suite('icaltime', function() {
       minute: 0,
       second: 0,
       leap_year: true,
-      day_of_week: ICAL.icaltime.SUNDAY,
-      day_of_year: 1,
+      dayOfWeek: Time.SUNDAY,
+      dayOfYear: 1,
       start_of_week: '20120101T000000',
       end_of_week: '20120107T000000',
       start_of_month: '20120101',
@@ -161,8 +328,8 @@ suite('icaltime', function() {
       minute: 0,
       second: 0,
       leap_year: false,
-      day_of_week: ICAL.icaltime.THURSDAY,
-      day_of_year: 1,
+      dayOfWeek: Time.THURSDAY,
+      dayOfYear: 1,
       start_of_week: '20081228T000000',
       end_of_week: '20090103T000000',
       start_of_month: '20090101',
@@ -175,7 +342,7 @@ suite('icaltime', function() {
 
     for (var datakey in test_data) {
       var data = test_data[datakey];
-      var dt = ICAL.icaltime.fromString(data.str);
+      var dt = Time.fromString(data.str);
       assert.equal(data.isDate, dt.isDate);
       assert.equal(data.year, dt.year);
       assert.equal(data.month, dt.month);
@@ -183,22 +350,22 @@ suite('icaltime', function() {
       assert.equal(data.hour, dt.hour);
       assert.equal(data.minute, dt.minute);
       assert.equal(data.second, dt.second);
-      assert.equal(data.leap_year, ICAL.icaltime.is_leap_year(dt.year));
-      assert.equal(data.day_of_week, dt.day_of_week());
-      assert.equal(data.day_of_year, dt.day_of_year());
+      assert.equal(data.leap_year, Time.is_leap_year(dt.year));
+      assert.equal(data.dayOfWeek, dt.dayOfWeek());
+      assert.equal(data.dayOfYear, dt.dayOfYear());
       assert.equal(data.start_of_week, dt.start_of_week());
       assert.equal(data.end_of_week, dt.end_of_week());
       assert.equal(data.start_of_month, dt.start_of_month());
       assert.equal(data.end_of_month, dt.end_of_month().toString());
       assert.equal(data.start_of_year, dt.start_of_year());
       assert.equal(data.end_of_year, dt.end_of_year());
-      assert.equal(data.start_doy_week, dt.start_doy_week(ICAL.icaltime.SUNDAY));
-      assert.equal(data.week_number, dt.week_number(ICAL.icaltime.SUNDAY));
+      assert.equal(data.start_doy_week, dt.start_doy_week(Time.SUNDAY));
+      assert.equal(data.week_number, dt.week_number(Time.SUNDAY));
       // TODO nth_weekday
 
-      dt = new ICAL.icaltime();
+      dt = new Time();
       dt.resetTo(data.year, data.month, data.day, data.hour, data.minute,
-                 data.second, ICAL.icaltimezone.utc_timezone);
+                 data.second, Timezone.utc_timezone);
       assert.equal(data.year, dt.year);
       assert.equal(data.month, dt.month);
       assert.equal(data.day, dt.day);
