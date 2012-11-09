f71a027c56252159fd4aaaeef21c4ffe0028054b
James Lal <james@lightsofapollo.com>
2012-11-08 23:26:36 -0800

    Component API v2

---
+++ test/mocha/period_test.js
@@ -0,0 +1,75 @@
+suite('ical/duration', function() {
+
+  suite('#fromString', function() {
+    var assertions = {};
+
+    function verify(string, data) {
+      var key;
+      test('parse: "' + string + '"', function() {
+        var subject = ICAL.icalperiod.fromString(string);
+
+        if ('start' in data) {
+          assert.hasProperties(
+            subject.start,
+            data.start,
+            'start property'
+          );
+        }
+
+        if ('end' in data) {
+          assert.hasProperties(
+            subject.end,
+            data.end,
+            'end property'
+          );
+        }
+
+        if ('duration' in data) {
+          assert.hasProperties(
+            subject.duration,
+            data.duration,
+            'duration property'
+          );
+        }
+      });
+    }
+
+    verify('19970101T180000Z/19970102T070000Z', {
+      duration: null,
+
+      start: {
+        year: 1997,
+        month: 1,
+        day: 1,
+        hour: 18,
+        minute: 0,
+        second: 0
+      },
+
+      end: {
+        year: 1997,
+        month: 1,
+        day: 2,
+        hour: 07
+      }
+    });
+
+    verify('19970101T180000Z/PT5H30M', {
+      start: {
+        year: 1997,
+        month: 1,
+        day: 1,
+        hour: 18
+      },
+
+      duration: {
+        isNegative: false,
+        hours: 5,
+        minutes: 30
+      }
+    });
+
+  });
+
+});
+
