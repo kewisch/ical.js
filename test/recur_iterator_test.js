7b8d59d6cded33d08da617ca42696755fadd6587
Arun Pandian <apandian@groupon.com>
2017-07-28 19:02:32 -0700

    Fix for Multi-day weekly recurring event missing first instance

--- test/recur_iterator_test.js
+++ test/recur_iterator_test.js
@@ -839,6 +839,31 @@ suite('recur_iterator', function() {
           '2015-01-01T08:00:00'
         ]
       });
+
+      //Weekly Sunday, Monday, Tuesday with count=3
+      testRRULE('FREQ=WEEKLY;COUNT=3;BYDAY=MO,SU,TU', {
+              dtStart: '2017-07-30',
+              byCount: true,
+              dates: [
+                '2017-07-30',
+                '2017-07-31',
+                '2017-08-01'
+              ]
+      });
+
+      //Weekly Sunday, Wednesday with count=5
+      testRRULE('FREQ=WEEKLY;COUNT=5;BYDAY=SU,WE', {
+              dtStart: '2017-04-23',
+              byCount: true,
+              dates: [
+                '2017-04-23',
+                '2017-04-26',
+                '2017-04-30',
+                '2017-05-03',
+                '2017-05-07'
+              ]
+      });
+
       //yearly, byMonth, byweekNo
       /* TODO BYWEEKNO is not well supported
       testRRULE('FREQ=YEARLY;BYMONTH=6,9;BYWEEKNO=23', {
