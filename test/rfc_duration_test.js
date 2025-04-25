aff781e2976b3070516242fa543dac9941fd8cc5
Akshith Pulgam <apulgam@umich.edu>
2025-04-25 13:46:05 -0400

    Added more test cases to test for events where there is more than 7 days. This tests that we don't get weeks and time.

--- test/rfc_duration_test.js
+++ test/rfc_duration_test.js
@@ -18,10 +18,21 @@
     assert.equal(d.toString(), "PT5H30M5S");
    });
  
-   test("days + time → P1DT2H", function() {
+   test("1 day + 2 hours → P1DT2H", function() {
     let Duration = new ICAL.Duration();
     let d = Duration.fromSeconds(86400 + 2 * 3600);
     assert.equal(d.toString(), "P1DT2H");
    });
+
+   test("9 days → P9D", function() {
+    let Duration = new ICAL.Duration();
+    let d = Duration.fromSeconds(9 * 86400);
+    assert.equal(d.toString(), "P9D");
+   });
+
+   test("10 days + 2 hours → P10DT2H", function() {
+    let Duration = new ICAL.Duration();
+    let d = Duration.fromSeconds(10 * 86400 + 2 * 3600);
+    assert.equal(d.toString(), "P10DT2H");
+   });
  });
- 
